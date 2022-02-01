6f7175e85c2e1dfb408f7d1fdb9ceaf7b4873c10
Aarush Jadhav <89852236+AarushJadhav@users.noreply.github.com>
2022-02-01 17:45:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var player, player_Img;
+var obstacle, obstacle_Img
 var bg_Image;
 
 function preload() {
   player_Img = loadAnimation("./images/running_1.png", "./images/running_2.png", "./images/running_3.png", "./images/running_4.png", "./images/running_5.png", "./images/running_6.png");
+  obstacle_Img = loadImage("./images/obstacle_1.jpg");
   bg_Image = loadImage("./images/bg_Img.gif");
 }
 
@@ -16,6 +18,26 @@ function setup() {
 
 function draw() {
   background(bg_Image);
+  spawnObstacles();
+  test();
   drawSprites();
 }
 
+function spawnObstacles() {
+  if (frameCount % 60 === 0) {
+   obstacle = createSprite(0, 300, 40, 10);
+   obstacle.addImage(obstacle_Img);
+   //obstacle.y = Math.round(random(10,600));
+   obstacle.scale = 0.1;
+   obstacle.velocityY = 3;
+
+
+    
+   obstacle.lifetime = 300;
+  }
+}
+
+function test() {
+  var obs = createSprite(200, 200, 50, 50);
+  obs.velocityX = -2;
+}
